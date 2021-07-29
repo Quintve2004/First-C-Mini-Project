f74c0854cd6bcdd4f42e8481960c41b9034a6419
Quint van elburg <Quintv@nelburg.nl>
2021-07-29 23:02:07 +0200

    menu commit

--- Program.cs
+++ Program.cs
@@ -4,28 +4,43 @@ namespace Review
 {
   class Program
   {
-    static void Main(string[] args)
+    static void Main()
     {
+        
         Console.WriteLine("Welcome To The ATM! \nType the menu number for waht you need\n1) Login\n2) Append CreditCard");
         string answerATM = Console.ReadLine();
-        
+        IDictionary<int, int> creditCards = new Dictionary<int, int>(){
+            {09876, 1234},
+            {12345, 4321}
+        };
+        Console.WriteLine(creditCards);
         if(answerATM == "1"){
-            Login();
+            Login(creditCards);
+        }
+        if(answerATM == "2"){
+            AppendCreditCard(creditCards);
         }
 
     } //static void main
 
-    static void Menu(int creditCardNUmber){
+    static void AppendCreditCard(IDictionary<int, int> creditCards){
+        Console.WriteLine("Type your CreditCard Number: ");
+        int newCreditCardNUmber = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Type your CreditCard Password: ");
+        int newCreditCardPassword = Convert.ToInt32(Console.ReadLine());
+        creditCards.Add(new KeyValuePair<int, int>(newCreditCardNUmber, newCreditCardPassword));
+        Console.WriteLine(creditCards);
+        Main();
+
+    }
+    static void MenuIN(int creditCardNUmber){
         Console.WriteLine($"Welcome \nYour Creditcard number is {creditCardNUmber}");
     }
 
-    static void Login(){
+    static void Login(IDictionary<int, int> creditCards){
         bool i = true;
         while(i == true){
-            IDictionary<int, int> creditCards = new Dictionary<int, int>(){
-            {09876, 1234},
-            {12345, 4321}
-        };
+
             int creditCardPassword = 0;
             Console.WriteLine("Type your Credit Card Number");
             int creditCardNUmber = Convert.ToInt32(Console.ReadLine());
@@ -42,7 +57,7 @@ namespace Review
 
             if(creditCards[creditCardNUmber] == creditCardPassword){
                 Console.WriteLine("Succeed!");
-                Menu(creditCardNUmber);
+                MenuIN(creditCardNUmber);
                 i = false;
             }
         }
